bcf1c18d753bfbb3138e67f885ce93b17898803b
Aline Rosa <alinerosa_s@hotmail.com>
2025-05-24 02:19:48 -0300

    Definindo tipos de produtos

---
+++ GestorDeEstoque/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorDeEstoque
+{
+    abstract class Produto //Serve como pai, mas não é possivel criar instâncias dela
+    {
+        public string nome;
+        public float preco;
+    }
+}
