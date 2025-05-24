6337c124cc8a0cb00d33b0c703b6a081d3f0413d
Aline Rosa <alinerosa_s@hotmail.com>
2025-05-24 02:12:54 -0300

    Criando menu da aplicação

---
+++ GestorDeEstoque/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace GestorDeEstoque
+{
+    internal class Program
+    {
+        enum Menu { Listar = 1, Adicionar, Remover, Entrada, Saida, Sair }
+        static void Main(string[] args)
+        {
+            bool escolheuSair = false;
+            while (escolheuSair == false)
+            {
+                Console.WriteLine("Sistema de Estoque");
+                Console.WriteLine();
+                Console.WriteLine("1 - Listar\n2 - Adicionar\n3 - Remover\n4 - Entrada\n5 - Saida\n6 - Sair");
+                string opStr = Console.ReadLine();
+                int opInt = int.Parse(opStr);
+
+                Menu escolha = (Menu)opInt;
+
+                if (opInt > 0 && opInt < 7)
+                {
+                    switch (escolha)
+                    {
+                        case Menu.Listar:
+                            break;
+                        case Menu.Adicionar:
+                            break;
+                        case Menu.Remover:
+                            break;
+                        case Menu.Entrada:
+                            break;
+                        case Menu.Saida:
+                            break;
+                        case Menu.Sair:
+                            escolheuSair = true;
+                            break;
+                    }
+                }
+                else
+                {
+                    escolheuSair = true;
+                }
+            }
+        }
+    }
+}
